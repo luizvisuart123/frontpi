ee1dc196263fd5bc7d68a9a0229630ab99106f06
Luiz Carlos dos Santos Neto <c148697@corp.caixa.gov.br>
2024-05-19 06:21:33 -0300

    calculando o imc e gravando no banco de dados

--- src/pages/FormImc.js
+++ src/pages/FormImc.js
@@ -1,53 +1,128 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Bar } from 'react-chartjs-2'; // Importe o componente de gráfico de barras
+import React, { useState, useContext } from 'react';
+import { TokenContext } from '../configuracao/TokenContext';
 
-function FormImc() {
-  const [peso, setPeso] = useState('');
-  const [altura, setAltura] = useState('');
-  const [sexo, setSexo] = useState('');
-  const [resultado, setResultado] = useState('');
+const FormImc = () => {
+  const [peso, setPeso] = useState(0); // Estado para o peso do paciente
+  const [altura, setAltura] = useState(0); // Estado para a altura do paciente
+  const [imc, setImc] = useState(0); // Estado para o valor do IMC calculado
+  const [classificacao, setClassificacao] = useState(''); // Estado para a classificação do IMC
+  const { getToken } = useContext(TokenContext);
 
-  const calcularIMC = async () => {
-    try {
-      const response = await axios.get(`http://localhost:8080/api/imc?peso=${peso}&altura=${altura}&sexo=${sexo}`);
-      setResultado(response.data);
-    } catch (error) {
-      console.error('Erro ao buscar dados da API:', error.message);
+
+  const calcularImc = async () => {
+    const token = getToken() || '';
+    if (peso > 0 && altura > 0) {
+      const imcCalculado = peso / (altura * altura);
+      setImc(imcCalculado.toFixed(2)); // Arredondar para duas casas decimais
+
+      const classificacaoImc = determinarClassificacao(imcCalculado);
+      setClassificacao(classificacaoImc);
+
+      if (token) {
+        try {
+          const response = await fetch('http://localhost:8080/calculo_imc', {
+            method: 'POST',
+            headers: {
+              'Authorization': `Bearer ${token}`,
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              usuario: {
+                username: 'user_admin', // username fixo
+              },
+              dataCalculo: new Date().toISOString().slice(0, 10), // Data atual
+              imc: imcCalculado,
+              classificacaoIMC: classificacaoImc,
+            }),
+          });
+
+          const responseData = await response.json();
+          console.log('Resposta do backend:', responseData);
+        } catch (error) {
+          console.error('Erro ao enviar dados para o backend:', error);
+        }
+      } else {
+        console.error('Token não encontrado no contexto.');
+      }
     }
   };
 
-  // Dados do gráfico
-  const data = {
-    labels: ['IMC'],
-    datasets: [
-      {
-        label: 'IMC',
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderWidth: 1,
-        hoverBackgroundColor: 'rgba(75,192,192,0.8)',
-        hoverBorderColor: 'rgba(75,192,192,1)',
-        data: [resultado], // Use o valor do IMC calculado aqui
+  const determinarClassificacao = (imc) => {
+    if (imc < 18.5) {
+      return 'MAGREZA';
+    } else if (imc >= 18.5 && imc <= 24.9) {
+      return 'NORMAL';
+    } else if (imc >= 25 && imc <= 29.9) {
+      return 'SOBREPESO';
+    } else if (imc >= 30 && imc <= 34.9) {
+      return 'OBESIDADE1';
+    } else if (imc >= 35 && imc <= 39.9) {
+      return 'OBESIDADE1';
+    } else {
+      return 'OBESIDADE3';
+    }
+  };
+
+  const gerarJson = () => {
+    const dataAtual = new Date().toISOString().slice(0, 10); // Obter data atual
+    const resultado = {
+      usuario: {
+        username: 'user_admin',
       },
-    ],
+      data: dataAtual,
+      imc: imc,
+      classificacao: classificacao,
+    };
+
+    // Retornar o JSON como string
+    return JSON.stringify(resultado);
   };
 
   return (
     <div>
-      <h1>Calculadora de IMC</h1>
-      <input type="number" placeholder="Peso (kg)" value={peso} onChange={(e) => setPeso(e.target.value)} />
-      <input type="number" placeholder="Altura (m)" value={altura} onChange={(e) => setAltura(e.target.value)} />
-      <input type="text" placeholder="Sexo (M/F)" value={sexo} onChange={(e) => setSexo(e.target.value)} />
-      <button onClick={calcularIMC}>Calcular</button>
-      {resultado && (
+      <h2>Calculadora de IMC</h2>
+
+      <div>
+        <label>Peso (kg):</label>
+        <input
+          min="0"
+          value={peso}
+          onChange={(e) => {
+            const pesoValor = parseFloat(e.target.value) || 0; // Set to 0 if empty
+            setPeso(pesoValor);
+          }}
+        />
+      </div>
+
+      <div>
         <div>
-          <p>Resultado: {resultado}</p>
-          <Bar data={data} /> {/* Renderize o gráfico aqui */}
+          <label>Altura (m):</label>
+          <input
+            inputMode='number'
+            min="0"
+            max="3"
+            value={altura}
+            onChange={(e) => {
+              const alturaValor = parseFloat(e.target.value) || 0;
+              setAltura(alturaValor);
+            }}
+          />
+        </div>
+
+      </div>
+
+      <button onClick={calcularImc}>Calcular</button>
+
+      {imc > 0 && (
+        <div>
+          <p>IMC: {imc}</p>
+          <p>Classificação: {classificacao}</p>
         </div>
       )}
+
+      {gerarJson() && <pre>{gerarJson()}</pre>}
     </div>
   );
-}
+};
 
 export default FormImc;
